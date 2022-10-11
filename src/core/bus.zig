8ce9240d83523d96aa8cd58b6c945065fae37512
Michelle-Marie Schiller <michellemarie.schiller@outlook.com>
2022-10-11 16:31:58 +0200

    Add RDRAM, KPUTCHAR register
    TODO: Put RDRAM registers in own file

--- src/core/bus.zig
+++ src/core/bus.zig
@@ -33,8 +33,6 @@ const Kputchar = struct {
         }
 
         if (c == 0x0A) {
-            self.msg[self.idx] = 0;
-
             self.flush();
         } else {
             self.msg[self.idx] = c;
@@ -47,25 +45,39 @@ const Kputchar = struct {
     fn flush(self: *Kputchar) void {
         info("   [KPUTCHAR  ] {s}", .{self.msg});
 
+        var i: usize = 0;
+        while (i < 256) : (i += 1) {
+            self.msg[i] = 0;
+        }
+
         self.idx = 0;
     }
 };
 
 /// Memory base addresses
 const MemBase = enum(u32) {
+    Ram  = 0x0000_0000,
     Bios = 0x1FC0_0000,
 };
 
 /// Memory sizes
 const MemSize = enum(u32) {
-    Bios = 0x40_0000,
+    Ram  = 0x200_0000,
+    Bios = 0x040_0000,
 };
 
 // Memory arrays
-var bios: []u8 = undefined; // BIOS ROM
+var  bios: []u8 = undefined; // BIOS ROM
+var rdram: []u8 = undefined; // RDRAM
 
 /// KPUTCHAR
 var kputchar: Kputchar = Kputchar{};
+
+// RDRAM registers
+var  mchDrd: u32 = undefined;
+var mchRicm: u32 = undefined;
+
+var rdramSdevId: u32 = 0;
 
 /// Initializes the bus module
 pub fn init(allocator: Allocator, biosPath: []const u8) !void {
@@ -77,12 +89,21 @@ pub fn init(allocator: Allocator, biosPath: []const u8) !void {
 
     bios = try biosFile.reader().readAllAlloc(allocator, @enumToInt(MemSize.Bios));
 
+    rdram = try allocator.alloc(u8, @enumToInt(MemSize.Ram));
+
+    // Clear KPUTCHAR
+    var i: usize = 0;
+    while (i < 256) : (i += 1) {
+        kputchar.msg[i] = 0;
+    }
+
     info("   [Bus       ] Successfully loaded BIOS.", .{});
 }
 
 /// Deinitializes the bus module
 pub fn deinit(allocator: Allocator) void {
-    allocator.free(bios);
+    allocator.free(bios );
+    allocator.free(rdram);
 }
 
 /// Reads data from the system bus
@@ -91,11 +112,61 @@ pub fn read(comptime T: type, addr: u32) T {
 
     var data: T = undefined;
 
-    if (addr >= @enumToInt(MemBase.Bios) and addr < (@enumToInt(MemBase.Bios) + @enumToInt(MemSize.Bios))) {
+    if (addr >= @enumToInt(MemBase.Ram) and addr < (@enumToInt(MemBase.Ram) + @enumToInt(MemSize.Ram))) {
+        @memcpy(@ptrCast([*]u8, &data), @ptrCast([*]u8, &rdram[addr]), @sizeOf(T));
+    }
+    else if (addr >= @enumToInt(MemBase.Bios) and addr < (@enumToInt(MemBase.Bios) + @enumToInt(MemSize.Bios))) {
         @memcpy(@ptrCast([*]u8, &data), @ptrCast([*]u8, &bios[addr - @enumToInt(MemBase.Bios)]), @sizeOf(T));
     } else {
         switch (addr) {
-            0x1000_F130 => {
+            0x1000_F430 => {
+                info("   [Bus       ] Read ({s}) @ 0x{X:0>8} (MCH_RICM).", .{@typeName(T), addr});
+
+                data = 0;
+            },
+            0x1000_F440 => {
+                info("   [Bus       ] Read ({s}) @ 0x{X:0>8} (MCH_DRD).", .{@typeName(T), addr});
+
+                comptime {
+                    if (T != u32) {
+                        return 0;
+                    }
+                }
+
+                const sop = @truncate(u4, mchRicm >> 6);
+
+                if (sop == 0) {
+                    const sa = @truncate(u8, mchRicm >> 16);
+
+                    switch (sa) {
+                        0x21 => {
+                            info("   [RDRAM     ] Command 0x21 (Init).", .{});
+
+                            if (rdramSdevId < 2) {
+                                rdramSdevId += 1;
+
+                                data = 0x1F;
+                            } else {
+                                data = 0;
+                            }
+                        },
+                        0x40 => {
+                            info("   [RDRAM     ] Command 0x40 (DevId).", .{});
+
+                            data = mchRicm & 0x1F;
+                        },
+                        else => {
+                            err("  [RDRAM     ] Unhandled RDRAM command 0x{X:0>2}.", .{sa});
+
+                            assert(false);
+                        }
+                    }
+                } else {
+                    data = 0;
+                }
+            },
+            0x1000_F130,
+            0x1000_F400, 0x1000_F410 => {
                 warn("[Bus       ] Read ({s}) @ 0x{X:0>8} (Unknown).", .{@typeName(T), addr});
 
                 data = 0;
@@ -115,19 +186,58 @@ pub fn read(comptime T: type, addr: u32) T {
 pub fn write(comptime T: type, addr: u32, data: T) void {
     assert(T == u8 or T == u16 or T == u32 or T == u64 or T == u128);
 
-    switch (addr) {
-        0x1000_F180 => {
-            assert(T == u8);
+    if (addr >= @enumToInt(MemBase.Ram) and addr < (@enumToInt(MemBase.Ram) + @enumToInt(MemSize.Ram))) {
+        @memcpy(@ptrCast([*]u8, &rdram[addr]), @ptrCast([*]const u8, &data), @sizeOf(T));
+    } else {
+        switch (addr) {
+            0x1000_F180 => {
+                // info("   [Bus       ] Write ({s}) @ 0x{X:0>8} (KPUTCHAR) = 0x{X}.", .{@typeName(T), addr, data});
 
-            kputchar.write(@truncate(u8, data));
-        },
-        0x1000_F100, 0x1000_F120, 0x1000_F140, 0x1000_F150,
-        0x1000_F410, 0x1000_F480, 0x1000_F490,
-        0x1000_F500 => warn("[Bus       ] Write ({s}) @ 0x{X:0>8} (Unknown) = 0x{X}.", .{@typeName(T), addr, data}),
-        else => {
-            err("  [Bus       ] Unhandled write ({s}) @ 0x{X:0>8} = 0x{X}.", .{@typeName(T), addr, data});
+                comptime {
+                    if (T != u8) {
+                        return;
+                    }
+                }
 
-            assert(false);
+                kputchar.write(@truncate(u8, data));
+            },
+            0x1000_F430 => {
+                info("   [Bus       ] Write ({s}) @ 0x{X:0>8} (MCH_RICM) = 0x{X}.", .{@typeName(T), addr, data});
+
+                comptime {
+                    if (T != u32) {
+                        return;
+                    }
+                }
+
+                const  sa = @truncate(u8, data >> 16);
+                const sbc = @truncate(u4, data >>  6);
+
+                if ((sa == 0x21) and (sbc == 1) and ((mchDrd & (1 << 7)) == 0)) {
+                    rdramSdevId = 0;
+                }
+
+                mchRicm = data & ~@as(u32, 0x80000000);
+            },
+            0x1000_F440 => {
+                info("   [Bus       ] Write ({s}) @ 0x{X:0>8} (MCH_DRD) = 0x{X}.", .{@typeName(T), addr, data});
+
+                comptime {
+                    if (T != u32) {
+                        return;
+                    }
+                }
+
+                mchDrd = data;
+            },
+            0x1000_F100, 0x1000_F120, 0x1000_F140, 0x1000_F150,
+            0x1000_F400, 0x1000_F410, 0x1000_F420, 0x1000_F450, 0x1000_F460, 0x1000_F480, 0x1000_F490,
+            0x1000_F500 => warn("[Bus       ] Write ({s}) @ 0x{X:0>8} (Unknown) = 0x{X}.", .{@typeName(T), addr, data}),
+            else => {
+                err("  [Bus       ] Unhandled write ({s}) @ 0x{X:0>8} = 0x{X}.", .{@typeName(T), addr, data});
+
+                assert(false);
+            }
         }
     }
 }
