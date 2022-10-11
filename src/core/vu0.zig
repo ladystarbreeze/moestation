c45dbf707df05c35c1f4abd4de4df04728deacbf
Michelle-Marie Schiller <michellemarie.schiller@outlook.com>
2022-10-12 00:15:58 +0200

    Add ISWR

--- src/core/vu0.zig
+++ src/core/vu0.zig
@@ -26,7 +26,19 @@ const RegFile = struct {
     pub fn getVi(self: RegFile, idx: u4) u16 {
         return self.vi[idx];
     }
+
+    /// Sets a VI register
+    pub fn setVi(self: *RegFile, idx: u4, data: u16) void {
+        self.vi[idx] = data;
+
+        self.vi[0] = 0;
+    }
 };
+
+const doDisasm = true;
+
+/// VU0 mem
+pub var vuMem: []u8 = undefined;
 
 var regFile: RegFile = RegFile{};
 
@@ -55,9 +67,21 @@ pub fn getControl(comptime T: type, idx: u5) T {
     return data;
 }
 
+/// Writes VU data memory
+fn write(comptime T: type, addr: u12, data: T) void {
+    assert((addr + @sizeOf(T)) < 0x1000);
+
+    @memcpy(@ptrCast([*]u8, &vuMem[addr]), @ptrCast([*]const u8, &data), @sizeOf(T));
+}
+
 /// Sets a COP2 control register
 pub fn setControl(comptime T: type, idx: u5, data: T) void {
     switch (idx) {
+        0 ... 15 => {
+            info("   [VU0 (COP2)] Control register write ({s}) @ $VI{} = 0x{X:0>8}.", .{@typeName(T), idx, data});
+
+            regFile.setVi(@truncate(u4, idx), @truncate(u16, data));
+        },
         @enumToInt(ControlReg.Fbrst) => {
             info("   [VU0 (COP2)] Control register write ({s}) @ $FBRST = 0x{X:0>8}.", .{@typeName(T), data});
 
@@ -75,7 +99,72 @@ pub fn setControl(comptime T: type, idx: u5, data: T) void {
             }
         },
         else => {
+            err("  [VU0 (COP2)] Unhandled control register write ({s}) @ ${} = 0x{X:0>8}.", .{@typeName(T), idx, data});
 
+            assert(false);
         }
+    }
+}
+
+/// Get dest field
+fn getDest(instr: u32) u4 {
+    return @truncate(u4, instr >> 21);
+}
+
+/// Get dest string
+fn getDestStr(dest: u4) []const u8 {
+    return switch (dest) {
+        0x0 => "",
+        0x1 => "w",
+        0x2 => "z",
+        0x3 => "zw",
+        0x4 => "y",
+        0x5 => "yw",
+        0x6 => "yz",
+        0x7 => "yzw",
+        0x8 => "x",
+        0x9 => "xw",
+        0xA => "xz",
+        0xB => "xzw",
+        0xC => "xy",
+        0xD => "xyw",
+        0xE => "xyz",
+        0xF => "xyzw",
+    };
+}
+
+/// Get t field
+fn getRt(instr: u32) u5 {
+    return @truncate(u5, instr >> 16);
+}
+
+/// Get s field
+fn getRs(instr: u32) u5 {
+    return @truncate(u5, instr >> 11);
+}
+
+/// Integer Store
+pub fn iIswr(instr: u32) void {
+    const dest = getDest(instr);
+
+    const it = getRt(instr);
+    const is = getRs(instr);
+
+    assert(it < 16 and is < 16);
+
+    const addr = @truncate(u12, regFile.getVi(@truncate(u4, is)) << 4);
+    const data = regFile.getVi(@truncate(u4, it));
+
+    var i: u12 = 0;
+    while (i < 4) : (i += 1) {
+        if ((dest & (@as(u4, 1) << (3 - @truncate(u2, i)))) != 0) {
+            write(u32, addr +% (i * 4), @as(u32, data));
+        }
+    }
+
+    if (doDisasm) {
+        const destStr = getDestStr(dest);
+
+        info("   [VU0       ] ISWR.{s} VI[{}], (VI[{}]){s}", .{destStr, it, is, destStr});
     }
 }
