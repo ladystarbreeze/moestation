6a00b3ea99eab3ddbcf97e3ca28822bad6ad5e92
Michelle-Marie Schiller <michellemarie.schiller@outlook.com>
2022-10-11 17:18:59 +0200

    Add INTC stub

---
+++ src/core/intc.zig
@@ -0,0 +1,41 @@
+//!
+//! moestation - PlayStation 2 emulator written in Zig.
+//! Copyright (c) 2022 Michelle-Marie Schiller
+//!
+//! intc.zig - Interrupt controller
+//!
+
+const std = @import("std");
+
+const assert = std.debug.assert;
+
+const err  = std.log.err;
+const info = std.log.info;
+
+/// Interrupt sources
+const IntSource = enum(u4) {
+    Gs,
+    Sbus,
+    VblankStart,
+    VblankEnd,
+    Vif0,
+    Vif1,
+    Vu0,
+    Vu1,
+    Ipu,
+    Timer0,
+    Timer1,
+    Timer2,
+    Timer3,
+    Sfifo,
+    Vu0Watchdog,
+};
+
+// INTC registers
+var intcMask: u15 = 0;
+var intcStat: u15 = 0;
+
+/// Returns INTC_MASK
+pub fn getMask() u32 {
+    return @as(u32, intcMask);
+}
