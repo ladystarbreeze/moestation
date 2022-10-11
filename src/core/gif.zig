3024f6718a3cd8545025c563d0d8f599a1489d8d
Michelle-Marie Schiller <michellemarie.schiller@outlook.com>
2022-10-11 18:40:49 +0200

    Add GIF stub

---
+++ src/core/gif.zig
@@ -0,0 +1,45 @@
+//!
+//! moestation - PlayStation 2 emulator written in Zig.
+//! Copyright (c) 2022 Michelle-Marie Schiller
+//!
+//! gif.zig - Graphics Interface
+//!
+
+const std = @import("std");
+
+const assert = std.debug.assert;
+
+const err  = std.log.err;
+const info = std.log.info;
+
+/// GIF I/O
+const GifReg = enum(u32) {
+    GifCtrl  = 0x1000_3000,
+    GifMode  = 0x1000_3010,
+    GifStat  = 0x1000_3020,
+    GifTag0  = 0x1000_3040,
+    GifTag1  = 0x1000_3050,
+    GifTag2  = 0x1000_3060,
+    GifTag3  = 0x1000_3070,
+    GifCnt   = 0x1000_3080,
+    GifP3Cnt = 0x1000_3090,
+    GifP3Tag = 0x1000_30A0,
+};
+
+/// Writes data to GIF I/O
+pub fn write(addr: u32, data: u32) void {
+    switch (addr) {
+        @enumToInt(GifReg.GifCtrl) => {
+            info("   [GIF       ] Write @ 0x{X:0>8} (GIF_CTRL) = 0x{X:0>8}.", .{addr, data});
+
+            if ((data & 1) != 0) {
+                info("   [GIF       ] Resetting GIF.", .{});
+            }
+        },
+        else => {
+            err("  [GIF       ] Unhandled write @ 0x{X:0>8} = 0x{X:0>8}.", .{addr, data});
+
+            assert(false);
+        }
+    }
+}
