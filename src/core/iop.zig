c931ae8301ffe27a98f81c96879a34959ae676af
Michelle-Marie Schiller <michellemarie.schiller@outlook.com>
2022-10-26 00:59:37 +0200

    Add IOP stub

---
+++ src/core/iop.zig
@@ -0,0 +1,189 @@
+//!
+//! moestation - PlayStation 2 emulator written in Zig.
+//! Copyright (c) 2022 Michelle-Marie Schiller
+//!
+//! iop.zig - IOP interpreter
+//!
+
+const std = @import("std");
+
+const assert = std.debug.assert;
+
+const err  = std.log.err;
+const info = std.log.info;
+
+const bus = @import("bus.zig");
+
+/// Enable/disable disassembler
+var doDisasm = true;
+
+const resetVector: u32 = 0xBFC0_0000;
+
+/// Branch delay slot helper
+var inDelaySlot: [2]bool = undefined;
+
+/// Register aliases
+const CpuReg = enum(u5) {
+    R0 =  0, AT =  1, V0 =  2, V1 =  3,
+    A0 =  4, A1 =  5, A2 =  6, A3 =  7,
+    T0 =  8, T1 =  9, T2 = 10, T3 = 11,
+    T4 = 12, T5 = 13, T6 = 14, T7 = 15,
+    S0 = 16, S1 = 17, S2 = 18, S3 = 19,
+    S4 = 20, S5 = 21, S6 = 22, S7 = 23,
+    T8 = 24, T9 = 25, K0 = 26, K1 = 27,
+    GP = 28, SP = 29, S8 = 30, RA = 31,
+};
+
+/// IOP register file
+const RegFile = struct {
+    // GPRs
+    regs: [32]u32 = undefined,
+
+    // Program counters
+     pc: u32 = undefined,
+    cpc: u32 = undefined,
+    npc: u32 = undefined,
+
+    lo: u32 = undefined,
+    hi: u32 = undefined,
+
+    /// Returns GPR
+    pub fn get(self: RegFile, comptime T: type, idx: u5) T {
+        return self.regs[idx];
+    }
+
+    /// Sets GPR
+    pub fn set(self: *RegFile, comptime T: type, idx: u5, data: T) void {
+        self.regs[idx] = data;
+
+        self.regs[0] = 0;
+    }
+
+    /// Sets program counter
+    pub fn setPc(self: *RegFile, data: u32) void {
+        assert((data & 3) == 0);
+    
+        self.pc  = data;
+        self.npc = data +% 4;
+    }
+
+    /// Advances program counter
+    pub fn stepPc(self: *RegFile) void {
+        self.pc = self.npc;
+        self.npc +%= 4;
+    }
+};
+
+/// RegFile instance
+var regFile = RegFile{};
+
+/// Initializes IOP interpreter
+pub fn init() void {
+    regFile.setPc(resetVector);
+
+    // cop0.init();
+
+    info("   [IOP       ] Successfully initialized.", .{});
+}
+
+/// Translates virtual address to physical address
+fn translateAddr(addr: u32) u32 {
+    // NOTE: this is Kernel mode only!
+
+    var data: u32 = undefined;
+
+    switch (@truncate(u4, addr >> 28)) {
+        0x8 ... 0x9, 0xA ... 0xB => {
+            data = addr & 0x1FFF_FFFF;
+        },
+        0x0 ... 0x7, 0xC ... 0xF => {
+            err("  [IOP       ] Unhandled TLB mapped access @ 0x{X:0>8}.", .{addr});
+
+            assert(false);
+        },
+    }
+
+    return data;
+}
+
+/// Reads data from the system bus
+fn read(comptime T: type, addr: u32) T {
+    return bus.readIop(T, translateAddr(addr));
+}
+
+/// Fetches an instruction from memory and increments PC
+fn fetchInstr() u32 {
+    const instr = read(u32, regFile.pc);
+
+    regFile.stepPc();
+
+    return instr;
+}
+
+/// Writes data to the system bus
+fn write(comptime T: type, addr: u32, data: T) void {
+    bus.writeIop(T, translateAddr(addr), data);
+}
+
+/// Get Opcode field
+fn getOpcode(instr: u32) u6 {
+    return @truncate(u6, instr >> 26);
+}
+
+/// Get Funct field
+fn getFunct(instr: u32) u6 {
+    return @truncate(u6, instr);
+}
+
+/// Get 16-bit immediate
+fn getImm16(instr: u32) u16 {
+    return @truncate(u16, instr);
+}
+
+/// Get 26-bit offset
+fn getInstrIndex(instr: u32) u26 {
+    return @truncate(u26, instr);
+}
+
+/// Get Sa field
+fn getSa(instr: u32) u5 {
+    return @truncate(u5, instr >> 6);
+}
+
+/// Get Rd field
+fn getRd(instr: u32) u5 {
+    return @truncate(u5, instr >> 11);
+}
+
+/// Get Rs field
+fn getRs(instr: u32) u5 {
+    return @truncate(u5, instr >> 21);
+}
+
+/// Get Rt field
+fn getRt(instr: u32) u5 {
+    return @truncate(u5, instr >> 16);
+}
+
+/// Decodes and executes instructions
+fn decodeInstr(instr: u32) void {
+    const opcode = getOpcode(instr);
+
+    switch (opcode) {
+        else => {
+            err("  [IOP       ] Unhandled instruction 0x{X} (0x{X:0>8}).", .{opcode, instr});
+
+            assert(false);
+        }
+    }
+}
+
+/// Steps the IOP interpreter
+pub fn step() void {
+    regFile.cpc = regFile.pc;
+
+    inDelaySlot[0] = inDelaySlot[1];
+    inDelaySlot[1] = false;
+
+    decodeInstr(fetchInstr());
+}
