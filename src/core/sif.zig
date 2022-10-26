e7386c94eb21deda478bbc2462acbdb4cd78d6c6
Michelle-Marie Schiller <michellemarie.schiller@outlook.com>
2022-10-26 11:41:42 +0200

    Add SIF reads

--- src/core/sif.zig
+++ src/core/sif.zig
@@ -14,7 +14,7 @@ const info = std.log.info;
 const warn = std.log.warn;
 
 /// SIF registers (EE)
-const SifRegEe = enum(u32) {
+const SifReg = enum(u32) {
     SifMscom = 0x1000_F200,
     SifMsflg = 0x1000_F220,
     SifSmflg = 0x1000_F230,
@@ -25,24 +25,55 @@ const SifRegEe = enum(u32) {
 /// SIF registers
 var mscom: u32 = undefined;
 var msflg: u32 = undefined;
+var smflg: u32 = undefined;
+
+/// Reads data from SIF registers (from EE)
+pub fn read(addr: u32) u32 {
+    var data: u32 = undefined;
+
+    switch (addr) {
+        @enumToInt(SifReg.SifMscom) => {
+            info("   [SIF       ] Read @ 0x{X:0>8} (SIF_MSCOM).", .{addr});
+
+            data = mscom;
+        },
+        @enumToInt(SifReg.SifMsflg) => {
+            info("   [SIF       ] Read @ 0x{X:0>8} (SIF_MSFLG).", .{addr});
+
+            data = msflg;
+        },
+        @enumToInt(SifReg.SifSmflg) => {
+            //info("   [SIF       ] Read @ 0x{X:0>8} (SIF_SMFLG).", .{addr});
+
+            data = smflg;
+        },
+        else => {
+            err("  [SIF       ] Unhandled read @ 0x{X:0>8}.", .{addr});
+
+            assert(false);
+        }
+    }
+
+    return data;
+}
 
 /// Writes data to SIF registers (from EE)
-pub fn writeEe(addr: u32, data: u32) void {
+pub fn write(addr: u32, data: u32) void {
     switch (addr) {
-        @enumToInt(SifRegEe.SifMscom) => {
+        @enumToInt(SifReg.SifMscom) => {
             info("   [SIF       ] Write @ 0x{X:0>8} (SIF_MSCOM) = 0x{X:0>8}.", .{addr, data});
 
             mscom = data;
         },
-        @enumToInt(SifRegEe.SifMsflg) => {
+        @enumToInt(SifReg.SifMsflg) => {
             info("   [SIF       ] Write @ 0x{X:0>8} (SIF_MSFLG) = 0x{X:0>8}.", .{addr, data});
 
             msflg = data;
         },
-        @enumToInt(SifRegEe.SifCtrl) => {
+        @enumToInt(SifReg.SifCtrl) => {
             info("   [SIF       ] Write @ 0x{X:0>8} (SIF_CTRL) = 0x{X:0>8}.", .{addr, data});
         },
-        @enumToInt(SifRegEe.SifBd6) => {
+        @enumToInt(SifReg.SifBd6) => {
             warn("[SIF       ] Write @ 0x{X:0>8} (SIF_BD6) = 0x{X:0>8}.", .{addr, data});
         },
         else => {
