dfb5397158045801e646e62d84a66969d5a6462c
Michelle-Marie Schiller <michellemarie.schiller@outlook.com>
2022-10-28 12:18:26 +0200

    Add IOP DMAC register writes and reads

--- src/core/bus.zig
+++ src/core/bus.zig
@@ -18,15 +18,16 @@ const Allocator = std.mem.Allocator;
 const openFile = std.fs.cwd().openFile;
 const OpenMode = std.fs.File.OpenMode;
 
-const cpu   = @import("cpu.zig");
-const cdvd  = @import("cdvd.zig");
-const dmac  = @import("dmac.zig");
-const gif   = @import("gif.zig");
-const gs    = @import("gs.zig");
-const intc  = @import("intc.zig");
-const sif   = @import("sif.zig");
-const timer = @import("timer.zig");
-const vu0   = @import("vu0.zig");
+const cpu     = @import("cpu.zig");
+const cdvd    = @import("cdvd.zig");
+const dmac    = @import("dmac.zig");
+const dmacIop = @import("dmac_iop.zig");
+const gif     = @import("gif.zig");
+const gs      = @import("gs.zig");
+const intc    = @import("intc.zig");
+const sif     = @import("sif.zig");
+const timer   = @import("timer.zig");
+const vu0     = @import("vu0.zig");
 
 /// Memory base addresses
 const MemBase = enum(u32) {
@@ -282,17 +283,21 @@ pub fn readIop(comptime T: type, addr: u32) T {
 
         data = cdvd.read(addr);
     } else if (addr >= @enumToInt(MemBaseIop.Dma0) and addr < (@enumToInt(MemBaseIop.Dma0) + @enumToInt(MemSizeIop.Dma))) {
-        warn("[Bus (IOP) ] Read ({s}) @ 0x{X:0>8} (DMA).", .{@typeName(T), addr});
+        if (T != u32) {
+            @panic("Unhandled read @ DMAC I/O");
+        }
 
-        data = 0;
+        data = dmacIop.read(addr);
     } else if (addr >= @enumToInt(MemBaseIop.Timer1) and addr < (@enumToInt(MemBaseIop.Timer1) + @enumToInt(MemSizeIop.Timer))) {
         warn("[Bus (IOP) ] Read ({s}) @ 0x{X:0>8} (Timer).", .{@typeName(T), addr});
 
         data = 0;
     } else if (addr >= @enumToInt(MemBaseIop.Dma1) and addr < (@enumToInt(MemBaseIop.Dma1) + @enumToInt(MemSizeIop.Dma))) {
-        warn("[Bus (IOP) ] Read ({s}) @ 0x{X:0>8} (DMA).", .{@typeName(T), addr});
+        if (T != u32) {
+            @panic("Unhandled read @ DMAC I/O");
+        }
 
-        data = 0;
+        data = dmacIop.read(addr);
     } else if (addr >= @enumToInt(MemBase.Bios) and addr < (@enumToInt(MemBase.Bios) + @enumToInt(MemSize.Bios))) {
         @memcpy(@ptrCast([*]u8, &data), @ptrCast([*]u8, &bios[addr - @enumToInt(MemBase.Bios)]), @sizeOf(T));
     } else {
@@ -530,13 +535,13 @@ pub fn writeIop(comptime T: type, addr: u32, data: T) void {
 
         cdvd.write(addr, data);
     } else if (addr >= @enumToInt(MemBaseIop.Dma0) and addr < (@enumToInt(MemBaseIop.Dma0) + @enumToInt(MemSizeIop.Dma))) {
-        warn("[Bus (IOP) ] Write ({s}) @ 0x{X:0>8} (DMA) = 0x{X}.", .{@typeName(T), addr, data});
+        dmacIop.write(T, addr, data);
     } else if (addr >= @enumToInt(MemBaseIop.Timer0) and addr < (@enumToInt(MemBaseIop.Timer0) + @enumToInt(MemSizeIop.Timer))) {
         warn("[Bus (IOP) ] Write ({s}) @ 0x{X:0>8} (Timer) = 0x{X}.", .{@typeName(T), addr, data});
     } else if (addr >= @enumToInt(MemBaseIop.Timer1) and addr < (@enumToInt(MemBaseIop.Timer1) + @enumToInt(MemSizeIop.Timer))) {
         warn("[Bus (IOP) ] Write ({s}) @ 0x{X:0>8} (Timer) = 0x{X}.", .{@typeName(T), addr, data});
     } else if (addr >= @enumToInt(MemBaseIop.Dma1) and addr < (@enumToInt(MemBaseIop.Dma1) + @enumToInt(MemSizeIop.Dma))) {
-        warn("[Bus (IOP) ] Write ({s}) @ 0x{X:0>8} (DMA) = 0x{X}.", .{@typeName(T), addr, data});
+        dmacIop.write(T, addr, data);
     } else if (addr >= @enumToInt(MemBaseIop.Sio2) and addr < (@enumToInt(MemBaseIop.Sio2) + @enumToInt(MemSizeIop.Sio2))) {
         warn("[Bus (IOP) ] Write ({s}) @ 0x{X:0>8} (SIO2) = 0x{X}.", .{@typeName(T), addr, data});
     } else {
