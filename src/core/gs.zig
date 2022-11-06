13cf038ab0c0340a1ffc945705123eff5454567b
Michelle-Marie Schiller <michellemarie.schiller@outlook.com>
2022-11-06 18:31:29 +0100

    Add stub GS register writes

--- src/core/gs.zig
+++ src/core/gs.zig
@@ -18,6 +18,65 @@ const intc = @import("intc.zig");
 const IntSource = intc.IntSource;
 const IntSourceIop = intc.IntSourceIop;
 
+/// GS registers
+const GsReg = enum(u8) {
+    Prim       = 0x00,
+    Rgbaq      = 0x01,
+    St         = 0x02,
+    Uv         = 0x03,
+    Xyzf2      = 0x04,
+    Xyz2       = 0x05,
+    Tex01      = 0x06,
+    Tex02      = 0x07,
+    Clamp1     = 0x08,
+    Clamp2     = 0x09,
+    Fog        = 0x0A,
+    Xyzf3      = 0x0C,
+    Xyz3       = 0x0D,
+    AddrData   = 0x0E,
+    Tex11      = 0x14,
+    Tex12      = 0x15,
+    Tex21      = 0x16,
+    Tex22      = 0x17,
+    XyOffset1  = 0x18,
+    XyOffset2  = 0x19,
+    PrModeCont = 0x1A,
+    PrMode     = 0x1B,
+    TexClut    = 0x1C,
+    ScanMsk    = 0x22,
+    MipTbp11   = 0x34,
+    MipTbp12   = 0x35,
+    MipTbp21   = 0x36,
+    MipTbp22   = 0x37,
+    TexA       = 0x3B,
+    FogCol     = 0x3D,
+    TexFlush   = 0x3F,
+    Scissor1   = 0x40,
+    Scissor2   = 0x41,
+    Alpha1     = 0x42,
+    Alpha2     = 0x43,
+    Dimx       = 0x44,
+    Dthe       = 0x45,
+    ColClamp   = 0x46,
+    Test1      = 0x47,
+    Test2      = 0x48,
+    Pabe       = 0x49,
+    Fba1       = 0x4A,
+    Fba2       = 0x4B,
+    Frame1     = 0x4C,
+    Frame2     = 0x4D,
+    Zbuf1      = 0x4E,
+    Zbuf2      = 0x4F,
+    BitBltBuf  = 0x50,
+    TrxPos     = 0x51,
+    TrxReg     = 0x52,
+    TrxDir     = 0x53,
+    Hwreg      = 0x54,
+    Signal     = 0x60,
+    Finish     = 0x61,
+    Label      = 0x62,
+};
+
 /// GS privileged registers
 const PrivReg = enum(u32) {
     Pmode    = 0x1200_0000,
@@ -47,6 +106,45 @@ const  cyclesInit: i64 = cyclesLine * 480;
 
 /// Simple VBLANK cycle counter
 var cyclesToVblank = cyclesInit;
+
+// GS registers
+var gsRegs: [0x63]u64 = undefined;
+
+var vtxCount: i32 = 0;
+
+/// Writes data to a GS register
+pub fn write(addr: u8, data: u64) void {
+    if (addr > @enumToInt(GsReg.Label)) {
+        err("  [GS        ] Invalid GS register address 0x{X:0>2}.", .{addr});
+
+        assert(false);
+    }
+
+    info("   [GS        ] Write @ 0x{X:0>2} ({s}) = 0x{X:0>16}.", .{addr, @tagName(@intToEnum(GsReg, addr)), data});
+
+    gsRegs[addr] = data;
+}
+
+/// Writes data to a GS register (from GIF)
+pub fn writePacked(addr: u4, data: u128) void {
+    switch (addr) {
+        @enumToInt(GsReg.AddrData) => {
+            const reg = @truncate(u8, data >> 64);
+
+            write(reg, @truncate(u64, data));
+        },
+        else => {
+            err("  [GS        ] Unhandled PACKED write @ 0x{X} = 0x{X:0>32}.", .{addr, data});
+
+            assert(false);
+        }
+    }
+}
+
+/// Writes data to HWREG
+pub fn writeHwreg(data: u64) void {
+    info("  [GS        ] Write @ HWREG = 0x{X:0>16}.", .{data});
+}
 
 /// Writes data to privileged register
 pub fn writePriv(addr: u32, data: u64) void {
